ff15bdadfe871631513cc5db370b595081fc2d21
Guilherme Alves Pinheiro <guiapinheiro29@gmail.com>
2024-06-03 21:25:09 -0300

    adding the createBook function on the endpoint

--- src/main/java/com/pinheiro/Books/controllers/BookController.java
+++ src/main/java/com/pinheiro/Books/controllers/BookController.java
@@ -1,12 +1,11 @@
 package com.pinheiro.Books.controllers;
 
 import com.pinheiro.Books.domain.Book;
+import com.pinheiro.Books.dtos.BookRequestDTO;
 import com.pinheiro.Books.services.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.util.List;
@@ -31,5 +30,11 @@ public class BookController {
     public ResponseEntity<List<Book>> findAll(){
         List<Book> list = service.findAll();
         return ResponseEntity.ok().body(list);
+    }
+
+    @PostMapping
+    public ResponseEntity createBook(@RequestBody BookRequestDTO data){
+        service.createBook(data);
+        return ResponseEntity.ok().body(data);
     }
 }
